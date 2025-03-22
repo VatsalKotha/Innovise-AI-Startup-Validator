f2894b8b407fd98a3560d341549c841bb9bf8478
Meet Chavan <meetchavan24@gmail.com>
2025-03-22 12:42:31 +0530

    port of render

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ async def root():
 # Run the FastAPI application
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=10000, reload=True)
